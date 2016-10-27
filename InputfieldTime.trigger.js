e7a6d71ca22d46476e976ecd4253a2572e48aa94
Netcarver <id.github.com@gzap.org>
2016-10-27 21:49:51 +0100

    Attempt to close all clock pickers when repeaters are opened or closed

--- InputfieldTime.trigger.js
+++ InputfieldTime.trigger.js
@@ -9,3 +9,21 @@ function InputFieldTimePickerTrigger(id) {
 
     picker.toggle();
 }
+
+$(document).on('closed', '.InputfieldRepeaterItem', function(e) {
+    $('div.clockpicker').each(function(e){
+        var picker = $(this).data('clockpicker');
+        if (picker) {
+            picker.hide();
+        }
+    });
+});
+
+$(document).on('opened', '.InputfieldRepeaterItem', function(e) {
+    $('div.clockpicker').each(function(e){
+        var picker = $(this).data('clockpicker');
+        if (picker) {
+            picker.hide();
+        }
+    });
+});
